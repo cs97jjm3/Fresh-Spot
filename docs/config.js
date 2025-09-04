1fb04fd8d074022ba85e7b009c34221b17727ab7
James Murrell <murrell.james@gmail.com>
2025-09-04 10:20:34 +0100

    Create config.js

---
+++ docs/config.js
@@ -0,0 +1,10 @@
+<script>
+  window.FRESHSTOP_CONFIG = {
+    // Get a free key: https://openweathermap.org/api
+    OWM_KEY: "d352aa257042303e9a15c24cd336741d",
+
+    // Optional: Get a free key: https://openrouteservice.org/dev/#/signup
+    // Used only as fallback if OSRM fails.
+    ORS_KEY: "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6Ijk2YmQwNmYwMDg5YjQ2YTk4NGQ3NDlmYzY5OGFiODUxIiwiaCI6Im11cm11cjY0In0="
+  };
+</script>
